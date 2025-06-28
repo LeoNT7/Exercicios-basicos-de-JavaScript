714f2fa09662141f56aa274b398f59be5274d904
Leo <lbento026@gmail.com>
2025-06-28 02:44:23 -0300

    organização das pastas em razões de escalabilidade

---
+++ extras/-MathRandom/app.js
@@ -0,0 +1,12 @@
+let numeroAleatorio = Math.random();
+console.log(numeroAleatorio);
+
+// Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+
+let numeroInteiro1e10 = parseInt(Math.random()* 10 + 1);
+console.log(numeroInteiro1e10);
+
+//Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+
+let numeroInteiro1e1000 = parseInt(Math.random()* 1000 + 1);
+console.log(numeroInteiro1e1000);
